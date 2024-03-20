70db5a4bff6f2c76880f8a50f3c8d3dd8bb80475
mariem mathlouthi <mariemmathlouthi15@gmail.com>
2024-03-20 15:58:22 +0100

    add route signup,login

--- laravel/Backend/routes/api.php
+++ laravel/Backend/routes/api.php
@@ -19,4 +19,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::group(['prefix' => '/auth'], function () {
+    Route::post('/singUp', [authController::class, 'singUp']);
+    Route::post('/LoginUser', [authController::class, 'LoginUser']);
+    
+});
+
 
