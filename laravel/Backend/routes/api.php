bb7fb16c2edec6286d33add58ea81d0c00263b85
mariem mathlouthi <mariemmathlouthi15@gmail.com>
2024-04-19 17:12:29 +0100

    add route stage  GetAllStage

--- laravel/Backend/routes/api.php
+++ laravel/Backend/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\authController;
+use App\Http\Controllers\Stage\StageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,4 +24,7 @@ Route::group(['middleware' => 'cors'], function () {
     Route::post('/singupEtudiant', [authController::class, 'signUpEtudiant']);
     Route::post('/signupEntreprise', [authController::class, 'signUpEntreprise']);
     Route::post('/login', [authController::class, 'LoginUser']);
+});
+Route::group(['prefix'=>'/stage'],function(){
+    Route::get('/GetAllStage',[StageController::class,'GetAllStage']);
 });
