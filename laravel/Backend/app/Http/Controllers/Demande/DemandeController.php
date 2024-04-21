ef59a958e8d3acb1c22751f77eb2a6754a2b3fd8
mariem mathlouthi <mariemmathlouthi15@gmail.com>
2024-04-21 18:45:37 +0100

    add function AddDemande

--- laravel/Backend/app/Http/Controllers/Demande/DemandeController.php
+++ laravel/Backend/app/Http/Controllers/Demande/DemandeController.php
@@ -3,9 +3,23 @@
 namespace App\Http\Controllers\Demande;
 
 use App\Http\Controllers\Controller;
+use App\Models\Demande;
 use Illuminate\Http\Request;
 
 class DemandeController extends Controller
 {
-    //
+    public function AddDemande (Request $request){
+        $requestData = $request->all();
+        $new = new Demande();
+        $new->idEtudiant = $requestData['idEtudiant']; $new->idoffreDeStage = $requestData['idOffreDeStage']; $new->statut= $requestData['statut'];
+        $date = $requestData['DateSoumission'];;
+        $formattedDate
+        =
+        date('Y-m-d', strtotime($date));
+        $new->DateSoumission = $formattedDate; $new->cv = $requestData['cv']; $new->save();
+        return response()->json([
+        'message' => 'Demande Added successfully', 'check' => true,
+        ]);
+    }
+    
 }
