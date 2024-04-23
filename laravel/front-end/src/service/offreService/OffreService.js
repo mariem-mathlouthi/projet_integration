d9bd08e5ebb8e0ac925f0f93be8c81bfd88d3304
mariem mathlouthi <mariemmathlouthi15@gmail.com>
2024-04-23 16:24:56 +0100

    Add 'getOfferById' method to OffreService.

--- laravel/front-end/src/service/offreService/OffreService.js
+++ laravel/front-end/src/service/offreService/OffreService.js
@@ -4,5 +4,10 @@ export default {
     getAllOffre(){
         return axios.get("http://localhost:8000/api/getAllOffre");
     },
+    getoffreById(id) {
+        return axios.get(`http://localhost:8000/api/getoffreById/${id}`);
+
+    },
+    
     
 }
