554c759645dfa6d9207039171bd9425f6446c5cf
cyrine laribi <cyrinelaribi1@gmail.com>
2024-05-18 03:53:17 +0100

    Mise à jour : petite changement affichage

--- laravel/front-end/src/components/StudentDash/DetailsOffreStd.vue
+++ laravel/front-end/src/components/StudentDash/DetailsOffreStd.vue
@@ -18,11 +18,13 @@
               <h1 class="text-3xl font-bold text-gray-800">Détails de l'offre</h1>
             </header>
             <div class="bg-purple-100 rounded-lg shadow-md p-6" v-if="offre">
-              <h2 class="text-xl font-semibold text-gray-800 mb-4">{{ offre.titre }}</h2>
-              <p class="text-gray-600 mb-4">{{ offre.description }}</p>
+              <h3 class="text-lg font-semibold mb-2"><span class="text-blue-800 font-bold">Titre :</span> {{ offre.titre }}</h3>
+             
+              <p class="text-gray-700 mb-4"><span class="text-blue-800 font-bold">Description :</span> {{ offre.description }}</p>
+
               <div class="flex flex-col md:flex-row items-center justify-between mb-4">
                 <div class="flex items-center">
-                  <p class="text-gray-500">{{ offre.entrepriseName }}</p>
+                  <p class="text-gray-700 mb-4"><span class="text-blue-800 font-bold">Entreprise :</span> {{ offre.entrepriseName }}</p>
                 </div>
                 <!-- Bouton pour importer le CV -->
                 <label for="uploadFile1" class="mt-4 md:mt-0 bg-gray-800 hover:bg-gray-700 text-white text-sm px-4 py-2.5 outline-none rounded w-max cursor-pointer block font-[sans-serif]">
